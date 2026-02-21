0015557b12b632fe4ccf24a88755218e542db1ff
Kavin <kavinpriyandarrsan@gmail.com>
2026-02-20 21:26:37 -0500

    Ensure dump directory exists for CI

--- backup_courseweave.sh
+++ backup_courseweave.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -e
 
 DATE=$(date +%Y%m%d_%H%M)
 
+mkdir -p db/postgres_dumps
+
 pg_dump -h 34.23.27.68 \
-        -p 5432 \
-        -U courseweave_user \
-        -d courseweave \
-        -F c \
-        -f db/postgres_dumps/courseweave_$DATE.dump
+  -p 5432 \
+  -U courseweave_user \
+  -d courseweave \
+  -F c \
+  -f db/postgres_dumps/courseweave_$DATE.dump
